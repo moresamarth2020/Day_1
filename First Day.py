d6109e748e73a83e0cd716591b13d24993d7d43e
More Samarth Nagesh <115178193+moresamarth2020@users.noreply.github.com>
2025-04-25 10:28:41 +0530

    Add files via upload

---
+++ First Day.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Our First Program
+
+# In[4]:
+
+
+print("Hello World")
+
+
+# In[5]:
+
+
+print("5")
+
+
+# In[6]:
+
+
+print(5+3)
+
+
+# In[7]:
+
+
+print(3-2)
+
+
+# In[8]:
+
+
+print(4*5)
+
+
+# In[9]:
+
+
+print(20/2)
+
+
+# In[ ]:
+
+
+
+
